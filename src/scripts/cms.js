dfdd35f2d42f81cb762e65824c20e6af58d983dd
Sam Brunno <sam.brunno@isobar.com>
2020-08-14 12:23:50 +1000

    - test cms previews disable

--- src/scripts/cms.js
+++ src/scripts/cms.js
@@ -3,20 +3,7 @@ import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 // eslint-disable-next-line import/no-unresolved
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../scss/main.scss";
+// import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../scss/main.scss";
 
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import PagesPreview from "./cms-preview-templates/pages";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
-
-CMS.registerPreviewStyle(styles, {raw: true});
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("pages", PagesPreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+// CMS.registerPreviewStyle(styles, {raw: true});
 CMS.init();
